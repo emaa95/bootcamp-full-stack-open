fef34210efba04d25aa3c21800d76a6e8b0049b2
emaa95 <chiletti.ema@gmail.com>
2024-02-13 12:28:41 -0300

    Exercise 2.12: The Phonebook step 7
    done

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -31,9 +31,13 @@ const App = () => {
     if (foundPerson) {
       window.alert(`${newName} is already added to phonebook`)
     } else {
-      setPersons(persons.concat(personObject))
-      setNewName('')
-      setNewNumber('')
+      axios
+      .post('http://localhost:3001/persons', personObject)
+      .then(response => {
+        setPersons(persons.concat(response.data))
+        setNewName('')
+        setNewNumber('')
+      })
     }
   }
 
