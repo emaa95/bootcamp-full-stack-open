3d9bf284dbc7922bdacae59edf75268e8bc083f0
emaa95 <chiletti.ema@gmail.com>
2024-05-21 18:05:10 -0300

    Exercises 5.3 and 5.4 Blog List Frontend
    step 3 and 4. done

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -2,12 +2,18 @@ import { useState, useEffect } from "react";
 import Blog from "./components/Blog";
 import blogService from "./services/blogs";
 import loginService from "./services/login"
+import Notification from "./components/Notification";
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [user, setUser] = useState(null)
+  const [newTitle, setNewTitle] = useState("")
+  const [newAuthor, setNewAuthor] = useState("")
+  const [newUrl, setNewUrl] = useState("")
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [succesMessage, setSuccessMessage] = useState(null)
 
   useEffect(() => {
     const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser')
@@ -33,8 +39,12 @@ const App = () => {
       setUser(user)
       setUsername('')
       setPassword('')
+      
     } catch (e) {
-        console.log("wrong credentials")
+        setErrorMessage('Wrong credentials...')
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
     }
   };
 
@@ -42,10 +52,33 @@ const App = () => {
     window.localStorage.removeItem('loggedNoteappUser')
     setUser(null)
   }
-    
+  
+  const addBlog = (event) => {
+    event.preventDefault()
+    const blogObject = {
+      title: newTitle,
+      author: newAuthor,
+      url: newUrl
+    }
+  
+    blogService
+      .create(blogObject)
+        .then(returnedBlog => {
+        setBlogs(blogs.concat(returnedBlog))
+        setNewTitle('')
+        setNewAuthor('')
+        setNewUrl('')
+        setSuccessMessage(`${newTitle} was created successfully`)
+        setTimeout(() => {
+          setSuccessMessage(null)
+        }, 5000)
+      })
+  }
       {if (user === null ){
         return(
+          
           <div>
+          <Notification error={errorMessage}/>
           <form onSubmit={handleLogin}>
             <div>
             username
@@ -54,7 +87,7 @@ const App = () => {
             value={username}
             name="Username"
             onChange={({ target }) => setUsername(target.value)}
-          />
+            />
         </div>
         <div>
           password
@@ -73,7 +106,39 @@ const App = () => {
       return(
         <div>
         <h2>blogs</h2>
+        <Notification success={succesMessage}/>
         {<p>{`${user.username} logged in`} <button onClick={handleLogout}>logout</button></p>}
+        <h3>create new</h3>
+        <form onSubmit={addBlog}>
+          <div>
+          title
+            <input
+            type="text"
+            value={newTitle}
+            name="Title"
+            onChange={({ target }) => setNewTitle(target.value)}
+            />
+          </div>
+          <div>
+          author
+            <input
+            type="text"
+            value={newAuthor}
+            name="Author"
+            onChange={({ target }) => setNewAuthor(target.value)}
+            />
+          </div>
+          <div>
+          url
+            <input
+            type="text"
+            value={newUrl}
+            name="Url"
+            onChange={({ target }) => setNewUrl(target.value)}
+            />
+          </div>
+          <button type="submit">create</button>{" "}
+        </form>
         { 
         blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
---
+++ part5/bloglist-frontend/src/components/Notification.jsx
@@ -0,0 +1,26 @@
+import './Notification.css'
+
+const Notification = ({ error, success }) => {
+    
+
+    if (error === null || success === null) {
+      return null
+    }
+    
+    if (error) {
+        return (
+            <div className="error">
+              {error}
+            </div>
+        )
+    } else {
+        return (
+            <div className="succes">
+              {success}
+            </div>
+          )
+    }
+    
+  }
+  
+  export default Notification
