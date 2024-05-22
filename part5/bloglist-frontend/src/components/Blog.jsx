4824508e6ff8e5ed1d64f071fd04e4c50e839647
emaa95 <chiletti.ema@gmail.com>
2024-05-22 09:24:33 -0300

    Exercise 5.7 Blog List Frontend, step 7
    done

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,39 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [blogVisible, setBlogVisible] = useState(false)
+
+  const hideWhenVisible = { display: blogVisible ? 'none' : '' }
+  const showWhenVisible = { display: blogVisible ? '' : 'none' }
+
+  if (blogVisible === false) {
+  return(
+  <div style={blogStyle}>
+  <div style={hideWhenVisible}>
+    {blog.title} <button onClick={() => setBlogVisible(true)}>view</button>
+  </div> 
+  </div>
+  ) 
+  } else {
+    return(
+      <div style={blogStyle}>
+      <div style={showWhenVisible}>
+        {blog.title} <button onClick={() => setBlogVisible(false)}>hide</button>
+        <p>{blog.url}</p>
+        <p>likes {blog.likes} <button>like</button></p>
+        <p>{blog.author}</p>
+      </div>
+      </div>
+    )
+  }
+}
 
 export default Blog
