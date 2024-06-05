9214d1d48e73ce3547ee57e2af9a620e96cdb965
emaa95 <chiletti.ema@gmail.com>
2024-06-04 21:12:44 -0300

    Exercise 5.18: Blog List End To End Testing, step 2
    done

--- part5/blogs-e2e/tests/blog_app.spec.js
+++ part5/blogs-e2e/tests/blog_app.spec.js
@@ -16,4 +16,26 @@ describe('Blog app', () => {
     // Verifica que el botón de inicio de sesión esté presente y visible
     await expect(page.getByRole('button', { name: 'log in' })).toBeVisible();
   })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await page.getByTestId('username').fill('test')
+      await page.getByTestId('password').fill('test')
+      await page.getByRole('button', { name: 'log in' }).click()
+      await expect(page.getByText('test logged in')).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+    await page.getByTestId('username').fill('mluukkai')
+    await page.getByTestId('password').fill('wrong')
+    await page.getByRole('button', { name: 'log in' }).click()
+    
+    await page.waitForSelector('.error')
+    const errorDiv = page.locator('.error')
+
+    await expect(errorDiv).toContainText('Wrong credentials...')
+    await expect(errorDiv).toHaveCSS('border-style', 'solid')
+    await expect(errorDiv).toHaveCSS('color', 'rgb(255, 0, 0)')
+    })
+  })
 })
