312b369b6d19f46019cead4fcda1094f0f7fc80c
emaa95 <chiletti.ema@gmail.com>
2024-06-12 18:05:04 -0300

    Exercise 6.4: Anecdotes, step 2
    done

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -12,6 +12,16 @@ const App = () => {
     });
   }
 
+  const addAnecdote = (event) => {
+    event.preventDefault()
+    const content = event.target.content.value
+    event.target.content.value = ''
+    dispatch({
+      type: 'NEW_ANECDOTE',
+      payload: content
+    })
+  }
+
   return (
     <div>
       <h2>Anecdotes</h2>
@@ -27,9 +37,9 @@ const App = () => {
         </div>
       )}
       <h2>create new</h2>
-      <form>
-        <div><input /></div>
-        <button>create</button>
+      <form onSubmit={addAnecdote}>
+        <div><input name='content' /></div>
+        <button type='submit'>create</button>
       </form>
     </div>
   )
