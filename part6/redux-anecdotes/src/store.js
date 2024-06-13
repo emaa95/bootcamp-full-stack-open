a2a5272170ed82a12d67ec6fa6a9331e05b41142
emaa95 <chiletti.ema@gmail.com>
2024-06-12 23:58:11 -0300

    Exercise 6.10 Better Anecdotes, step 8
    done

---
+++ part6/redux-anecdotes/src/store.js
@@ -0,0 +1,13 @@
+// src/store.js
+import { configureStore } from '@reduxjs/toolkit';
+import anecdoteReducer from './reducers/anecdoteReducer';
+import filterReducer from './reducers/filterReducer';
+
+const store = configureStore({
+  reducer: {
+    anecdotes: anecdoteReducer,
+    filter: filterReducer,
+  },
+});
+
+export default store;
