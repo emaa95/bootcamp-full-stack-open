eadf3a630d8783b80c1af413be2173f79c12ae7e
emaa95 <chiletti.ema@gmail.com>
2024-06-13 17:41:40 -0300

    Exercise 6.16 Anecdotes and the Backend, step 3
    done

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -4,14 +4,13 @@ import AnecdoteList from './components/AnecdoteList'
 import Filter from './components/Filter'
 import Notification from './components/Notification'
 import { useEffect } from 'react'
-import anecdoteServicer from './services/anecdotes'
-import { setAnecdotes } from './reducers/anecdoteReducer'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
 
 const App = () => {
   const dispatch = useDispatch()
   useEffect(() => {
-    anecdoteServicer.getAll().then(anecdotes => dispatch(setAnecdotes(anecdotes)))
-  })
+    dispatch(initializeAnecdotes())
+  }, [dispatch])
   return (
     <div>
       <h2>Anecdotes</h2>
